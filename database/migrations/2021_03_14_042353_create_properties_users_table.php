2b69e6d95c4309301cd53ce4bbfc7b15fa7328cd
Luis David Gonzalez Duerto <luisdgd07@gmail.com>
2021-03-20 11:17:52 -0400

    Propiedes2

--- database/migrations/2021_03_14_042353_create_properties_users_table.php
+++ database/migrations/2021_03_14_042353_create_properties_users_table.php
@@ -18,7 +18,6 @@ class CreatePropertiesUsersTable extends Migration
             $table->unsignedBigInteger('property_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
-
             $table->foreign('property_id')->references('id')->on('properties')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
