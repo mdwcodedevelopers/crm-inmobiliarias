f85cec36139734cc7dfaa27de19c28782629227a
Luis David Gonzalez Duerto <luisdgd07@gmail.com>
2021-03-20 11:08:57 -0400

    Propied

--- resources/js/components/Properties.vue
+++ resources/js/components/Properties.vue
@@ -170,7 +170,6 @@
           { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 4 },
           { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 4 },
           { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 4 },
-
         ],
         links: [
           'Home',
