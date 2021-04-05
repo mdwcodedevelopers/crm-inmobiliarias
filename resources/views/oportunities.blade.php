048939a337885fe47bfd1043000f7565d2df9ef0
Luis David Gonzalez Duerto <luisdgd07@gmail.com>
2021-04-04 22:55:08 -0400

    Fix merge

--- resources/views/oportunities.blade.php
+++ resources/views/oportunities.blade.php
@@ -3,15 +3,11 @@
 @section('content')
     <div class="">
         <div class="">
-<<<<<<< HEAD
             <nav-admin :title="'Oportunidades'"/>
-=======
-            <nav-admin title="Oportunidades" />
->>>>>>> deee9bd50dbe7da7cbae6768cbe8c1850ae1a9e9
         </div>
-        
-        <oportunities 
-            :users="{{$users}}" 
+
+        <oportunities
+            :users="{{$users}}"
             :status="{{$status}}"
         />
 
