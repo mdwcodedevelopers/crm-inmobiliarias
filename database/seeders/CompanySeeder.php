84b2ac17c335aaa5cc2743744d012a8c904545ac
severalreves <gabrielalexander2@gmail.com>
2021-04-15 02:26:38 -0400

    actualizacion de laravel

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('company')->insert([
+            'name' => '',
+            'information' => '',
+            'image_url' =>'',
+        ]);
+    }
+}
